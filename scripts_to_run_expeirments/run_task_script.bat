b0ac9d88b73a67d82cfc1495339396af15125522
Shuqi Liu <DanielleLiu@users.noreply.github.com>
2025-04-15 16:25:51 -0400

    Add bat script to run exp so that it'sm ore streamdlined
    - the bat script will set the path that will persist only for the lifespan of the script to avoid messing up env variables for other users
    - this is also more robust to avoid env path changes and may require elevated permission to set it, limits GUI interactions and likely more robust, click the .bat should do everything
    - also pause on error if needed so that we can see the error message
    - this also avoids having to type a long string while the participant is looking at the screen to set up the task/rest runs

---
+++ scripts_to_run_expeirments/run_task_script.bat
@@ -0,0 +1,9 @@
+set PYTHONPATH=C:\Users\shl187\Downloads\BrainWalk\MultiTaskBattery\
+echo %PYTHONPATH%
+
+C:\Users\shl187\Downloads\BrainWalk\Python390_working\Programs\Python\Python39\python.exe %PYTHONPATH%\experiments\braingait_experiment\run.py
+
+if errorlevel 1 (
+    echo An error occurred!
+    pause
+)
